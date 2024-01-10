2d7847242617f289cc1c08b7724dd22f1e26cc96
GloriaBimbi <gloria.bimbi1@libero.it>
2024-01-10 22:57:09 +0100

    feat milesone 1

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// STEP 1: dichiaro le variabili che mi servono:
+//- il tipo di offerta applicata e
+//- il prezzo del biglietto
+
+//- Nome e cognome dell'utente,
+const UserNameSurname = document.getElementById("name-surname").value;
+console.log("Username: " + UserNameSurname);
+
+//- il numero di chilometri che vuole eseguire,
+const lenghtInKm = parseInt(document.getElementById("travel-lenght").value);
+console.log("Km: " + lenghtInKm);
+
+//- età,
+const age = document.getElementById("age").value;
+console.log("Fascia d'età: " + age);
+
+//- il bottone "Calcola",
+const calcButton = document.getElementById("calc-button");
+console.log("Calcolo in corso...");
+
+// STEP 2: calcolo il prezzo del biglietto in base ai km percorsi
+const lenghtPrize = lenghtInKm * 0.21;
+console.log("Il prezzo sulla base dei km è: " + lenghtPrize);
+
+// STEP 3: calcolo il prezzo sulla base dell'età dell'utente
+let finalPrize = 0;
+let finalPrizeFixed = 0;
+
+if (age == "minor") {
+  finalPrize = lenghtPrize * 0.8;
+  finalPrizeFixed = finalPrize.toFixed(2);
+  console.log("Il prezzo viene scontato a: " + finalPrizeFixed);
+} else if (age == "over65") {
+  finalPrize = lenghtPrize * 0.6;
+  finalPrizeFixed = finalPrize.toFixed(2);
+  console.log("Il prezzo viene scontato a: " + finalPrizeFixed);
+} else {
+  finalPrize = lenghtPrize;
+  finalPrizeFixed = finalPrize.toFixed(2);
+  console.log("Il prezzo rimane invariato a: " + finalPrizeFixed);
+}
+
+// STEP 4: stampo il prezzo finale del biglietto
+console.log("Il prezzo finale è di: " + finalPrizeFixed);
+document.getElementById("final-prize").innerText = finalPrizeFixed;
+
+//BONUS: faccio in modo che cliccando sul bottone "Calcola" appaia il biglietto
+calcButton.addEventListener("click", function () {});
--- readme.md
+++ readme.md
@@ -1,12 +1,10 @@
+# CALCOLA IL COSTO DEL BIGLIETTO DEL TRENO
+
 ## SOLUZIONE:
 
-- MILESTONE 1-2:
-  - collego due input e un bottone
-  - chiedo all'utente "Quanti chilometri vuoi percorrere?"
-  - chiedo all'utente "Quanti anni hai"
-  - calcolo il prezzo del biglietto sulla base del chilometraggio
-  - eventualmente applico uno sconto del 20% se l'utente è minorenne
-  - eventualmente applico uno sconto del 40% se l'utente è over 65
-  - stampo in console e a schermo il prezzo finale del biglietto
-- BONUS:
-- faccio in modo che si possa visualizzare il biglietto solo dopo che l'utente ha cliccato sul bottone
+- STEP 1: dichiaro le variabili che mi servono
+- STEP 2: calcolo il prezzo del biglietto in base ai km percorsi
+- STEP 3: calcolo il prezzo sulla base dell'età dell'utente
+- STEP 4: stampo il prezzo finale del biglietto
+- STEP 5: creo il biglietto del treno
+- BONUS: faccio in modo che si possa visualizzare il biglietto solo dopo che l'utente ha cliccato sul bottone
